8fa7177a1d49e1a875e85afd87b8d29c5b9a19d9
prerana2601 <155650129+prerana2601@users.noreply.github.com>
2024-08-07 11:27:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-function firstWord(s) {
-  // your code here
+// script.js
+
+function firstWord(str) {
+    // Handle the case of an empty string
+    if (str === '') {
+        return '';
+    }
+
+    // Find the index of the first space
+    const spaceIndex = str.indexOf(' ');
+
+    // If a space is found, return the substring up to the first space
+    if (spaceIndex !== -1) {
+        return str.substring(0, spaceIndex);
+    }
+
+    // If no space is found, return the entire string
+    return str;
 }
 
-// Do not change the code below
+function extractFirstWord() {
+    // Get the user input
+    const inputString = document.getElementById('inputString').value;
+    const resultElement = document.getElementById('result');
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+    // Compute the first word
+    const result = firstWord(inputString);
+
+    // Display the result
+    resultElement.textContent = result;
+}
